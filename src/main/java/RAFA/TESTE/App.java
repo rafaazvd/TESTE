8c82351fffa5addfe6099ba0e677433e381fa433
unknown <rafa-spdf@hotmail.com>
2015-11-25 18:08:10 -0200

    'commit'

---
+++ src/main/java/RAFA/TESTE/App.java
@@ -0,0 +1,13 @@
+package RAFA.TESTE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
